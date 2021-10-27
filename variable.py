8418afe83a97922670231f18b3105dcf39f44ab4
gintaku <93266540+gintaku@users.noreply.github.com>
2021-10-27 21:11:39 +0700

    first commit

---
+++ variable.py
@@ -0,0 +1,6 @@
+character_name = "Tom"
+character_age = "50"
+print("there once was a man named " +character_name)
+print("he was "+ character_age +" years old")
+print("he really like the name "+character_name+",")
+print("but didn't like being "+character_age)
